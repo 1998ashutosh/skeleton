28d69fa5c323f8eb1d76aacaaed071ff34869aae
ashutosh_bathe <ashutosh.b1332@gmail.com>
2024-12-12 00:38:15 +0100

    commit to add changes for data from 2020-2023

--- bubbles.js
+++ bubbles.js
@@ -1,9 +1,7 @@
 // --------------------------------------------------
 // TODO 2.2
 // --------------------------------------------------
-// TODO 2.2: define a function that accesses the number of dead and missing migrants from the data set called sizeValue
-//const sizeValue =;
-const sizeValue = d => d['Total Dead and Missing'];
+const sizeValue = d => d['Total Number of Dead and Missing'];
 // defines the maximum radius of a bubble
 const maxRadius = 15;
 
@@ -17,21 +15,11 @@ const Bubbles = ({
     }, [data, sizeValue]);
     
     // TODO 4.2: Memoization for size scale
-    // TODO 2.2: the area covered by a bubble should be scaled proportional to the sizeValue. define a sizeScale using d3.scaleSqrt or d3.scaleLinear (think about which one is a better fit)
-    //const sizeScale = 
-        // TODO 2.2: set the domain to the scale using the .domain function. the domain should start at zero and end at the maximum of the data (use sizeValue)
-        // TODO 2.2: set the range to the scale using the .range function. the range should start at zero and end at maxRadius
-
-    // TODO 2.2: return the bubbles svg definition
-    return (
-        // TODO 2.2 wrap everything that follows in a group element with className bubbleMarks
-            // TODO 2.2: enter a JS scope
-                // TODO 2.2: map the data to get each row and index
-                    // TODO 2.2: use the projection to get the x and y coordinates from the coords
-                    // TODO 2.2: return a circle with the key being the index, x and y the extracted coordinates and the radius passed through the sizeScale
-        
+    return (        
         <g className="bubbleMarks">
-        {data.map((d, i) => {
+            
+        {
+        data.map((d, i) => {
             const [x, y] = projection(d.coords);
             return (
                 <circle
